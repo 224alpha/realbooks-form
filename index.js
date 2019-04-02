4d099fece0c99089e9124e3929d3fc4e8748d444
224alpha <kirankumardas224@gmail.com>
2019-04-02 20:59:38 +0530

    first push

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const PORT = process.env.PORT || 5200
+const path =require("path")
+
+
+
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*',(req,res,next)=>{
+    res.sendFile('index.html');
+})
+
+
+
+app.listen(PORT,(err)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log('Running on port = '+PORT);
+});
